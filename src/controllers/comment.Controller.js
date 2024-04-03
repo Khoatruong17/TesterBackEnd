8d5bf3a47013430be8e14b03ad86692ce1f68ab8
KhoaTruong17 <truongndkgch190486@email.com>
2024-04-03 10:33:57 +0700

    update comment

--- src/controllers/comment.Controller.js
+++ src/controllers/comment.Controller.js
@@ -1,81 +1,88 @@
 const express = require("express");
 const Comments = require("../models/commentModel");
 const Contributions = require("../models/contributionModel");
-const User = require("../models/userModel")
+const User = require("../models/userModel");
 const commentController = {
-    //create comment
-    createComment: async (req, res) => {
-        try {
-            const contriButionExsts = await Contributions.findOne({ _id: req.body.contribution_id })
-            const userExists = await User.findOne({_id: req.body.user_id})
+  //create comment
+  createComment: async (req, res) => {
+    try {
+      const cookies =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MGJiNmY2YTdiMGRiZWE3OTc1NzExNiIsImVtYWlsIjoic3R1ZGVudDAxQGdtYWlsLmNvbSIsImdyb3VwV2l0aFJvbGUiOnsic1JvbGVzIjpbeyJ1cmwiOiIvdGVzdCIsImRlc2NyaXB0aW9uIjoiR2V0IGFsbCB1c2VyIn1dLCJncm91cCI6eyJfaWQiOiI2NWZhZTQ3NTg1MDkwNWYwNWYwZTIyODIiLCJub19ncm91cCI6NCwiZ3JvdXBfbmFtZSI6IlN0dWRlbnQiLCJkZXNjcmlwdGlvbiI6IkNhbiB1cGxvYWQgdGhpZXIgY29udHJpYnV0aW9ucyAiLCJjcmVhdGVkQXQiOiIyMDI0LTAzLTIwVDEzOjI4OjIxLjc5MFoiLCJ1cGRhdGVkQXQiOiIyMDI0LTAzLTIwVDEzOjI4OjIxLjc5MFoiLCJfX3YiOjB9fSwiaWF0IjoxNzEyMDUwNTM3fQ.us07_fwWfY758BNd9emerAft2SQ3fBKmvJXDiElh69s"; // your jwt token
+      const decoded = jwtAction.verifyToken(cookies);
+      const coordinator_id = decoded.id;
+      const contriButionExists = await Contributions.findOne({
+        _id: req.body.contribution_id,
+      });
 
-            if (!contriButionExsts) {
-                return res.status(404).json({error: "contribution id not found "})
-            }
-            if (!userExists) {
-                return res.status(404).json({error: "user id not found "})
-            }
-            const newComment = await new Comments({
-                contribution_id: contriButionExsts._id,
-                user_id: userExists._id,
-                comment: req.body.comment
-            });
-        
-            const comment = await newComment.save();
-            console.log("Add comment Successfully");
-            res.status(200).json(comment);
-        } catch (error) {
-            console.log("Error create comment: " + error);
-            res.status(500).json({ error: error.message });
-        }
-    },
+      if (!contriButionExists) {
+        return res.status(404).json({ error: "contribution id not found " });
+      }
 
+      const userExists = await User.findOne({ _id: coordinator_id });
+      if (!userExists) {
+        return res.status(404).json({ error: "user id not found " });
+      }
+      const newComment = await new Comments({
+        contribution_id: contriButionExists._id,
+        user_id: coordinator_id,
+        comment: req.body.comment,
+      });
 
-
-
-        // Get all the comments
-    // Get all the comments
-    getAllComment: async(req, res) =>{
-        try{
-            const comment = await Comments.find();
-            res.status(200).json(comment);
-            console.log("Get all comments successfully");
-        }catch(error){
-            console.log("Error get all comments: " + error);
-            res.status(500).json({ error: error.message });
-        }
-    },
-
-
-    // Delete a comment
-    deleteComment: async (req, res) => {
-        try {
-            const comment = await Comments.findByIdAndDelete(req.params.id);
-            if (!comment) {
-                return res.status(404).json({ message: "Comment not found" });
-            }
-            res.status(200).json({ message: "Comment deleted successfully" }); 
-            console.log(`Delete comment successfully`);
-        } catch (error) {
-            console.log("Error deleting comment: " + error);
-            res.status(500).json({ error: error.message });
-        }
-    },
-
-    // Update a comment
-    updateComment: async (req, res) => {
-        try {
-            const updatedComment = req.body.comment;
-            const comment = await Comments.findByIdAndUpdate(req.params.id, { comment: updatedComment }, { new: true });
-            if (!comment) {
-                return res.status(404).json({ message: "Cannot find comment" });
-            }
-            res.status(200).json(comment);
-            console.log(`Update comment successfully`);
-        } catch (error) {
-            console.log("Error update comment: " + error);
-            res.status(500).json({ error: error.message });
-        }
+      const comment = await newComment.save();
+      console.log("Add comment Successfully");
+      res.status(200).json(comment);
+    } catch (error) {
+      console.log("Error create comment: " + error);
+      res.status(500).json({ error: error.message });
     }
-}
+  },
+
+  // Get all the comments
+  // Get all the comments
+  getAllComment: async (req, res) => {
+    try {
+      const comment = await Comments.find();
+      res.status(200).json(comment);
+      console.log("Get all comments successfully");
+    } catch (error) {
+      console.log("Error get all comments: " + error);
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  // Delete a comment
+  deleteComment: async (req, res) => {
+    try {
+      const comment = await Comments.findByIdAndDelete(req.params.id);
+      if (!comment) {
+        return res.status(404).json({ message: "Comment not found" });
+      }
+      res.status(200).json({ message: "Comment deleted successfully" });
+      console.log(`Delete comment successfully`);
+    } catch (error) {
+      console.log("Error deleting comment: " + error);
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  // Update a comment
+  updateComment: async (req, res) => {
+    try {
+      const updatedComment = req.body.comment;
+      const comment = await Comments.findByIdAndUpdate(
+        req.params.id,
+        { comment: updatedComment },
+        { new: true }
+      );
+      if (!comment) {
+        return res.status(404).json({ message: "Cannot find comment" });
+      }
+      res.status(200).json(comment);
+      console.log(`Update comment successfully`);
+    } catch (error) {
+      console.log("Error update comment: " + error);
+      res.status(500).json({ error: error.message });
+    }
+  },
+};
 module.exports = commentController;
