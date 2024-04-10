ee46bfa9f3ab48c43546b6a2eaebc48dc59c0960
KhoaTruong17 <truongndkgch190486@email.com>
2024-04-10 10:41:14 +0700

    update build

--- build/controllers/contribution.Controller.js
+++ build/controllers/contribution.Controller.js
@@ -11,8 +11,7 @@ const Comments = require("../models/commentModel");
 const fs = require("fs").promises;
 const expressZip = require("express-zip");
 const path = require("path");
-
-const findUser = async (topic_id) => {
+const findUser = async topic_id => {
   try {
     const topic = await Topic.findById(topic_id);
     if (!topic) {
@@ -28,11 +27,12 @@ const findUser = async (topic_id) => {
     throw error;
   }
 };
-
 const returnEmail = async () => {
   try {
     const user = await findUser(req.body.topic_id);
-    const email = await User.findOne({ email: user.email });
+    const email = await User.findOne({
+      email: user.email
+    });
     if (!email) {
       throw new Error("Email not found");
     }
@@ -42,7 +42,6 @@ const returnEmail = async () => {
     throw error;
   }
 };
-
 const createContribution = async (req, res) => {
   try {
     if (!req.files || Object.keys(req.files).length === 0) {
@@ -57,31 +56,26 @@ const createContribution = async (req, res) => {
       return res.status(400).send("Invalid cookie. Please Login!!!");
     }
     const student_id = decoded.id;
-
     const student = await User.findById(student_id);
     if (!student) {
-      throw new Error(
-        "Student not found, please check token (take student_id by token)"
-      );
+      throw new Error("Student not found, please check token (take student_id by token)");
     }
     const faculty_id = student.faculty.faculty_id;
     if (!faculty_id) {
       throw new Error("The user does not have faculty_id, please check again");
     }
-
     const topic_id = req.body.topic_id;
     const topic = await Topic.findById(topic_id);
     if (!topic) {
       throw new Error("Topic not found, please check topic_id");
     }
     const email = "truongndkgch190486@fpt.edu.vn";
-
     const filePath = await uploadFile.postUploadMultipleFiles(req);
     let documents = [];
     let countSuccess;
     console.log(filePath);
     if (Array.isArray(filePath.detail)) {
-      documents = filePath.detail.map((detail) => detail.path);
+      documents = filePath.detail.map(detail => detail.path);
       countSuccess = filePath.countSuccess;
     } else if (filePath.DT.path) {
       documents = filePath.DT.path;
@@ -91,7 +85,6 @@ const createContribution = async (req, res) => {
       return res.status(400).send("Invalid file upload data.");
     }
     const currentDate = new Date();
-
     console.log(">>> File Path", documents);
     //console.log({filePath,currentDate,student_id,topic_id});
     const newContribution = new Contributions({
@@ -103,7 +96,7 @@ const createContribution = async (req, res) => {
       description: req.body.description,
       document: documents,
       submit_date: currentDate,
-      status: 0,
+      status: 0
     });
     const contribution = await newContribution.save();
     if (contribution) {
@@ -112,30 +105,30 @@ const createContribution = async (req, res) => {
       let email_status = await sendEmailMessage.sendEmail(email, text);
       console.log(email_status);
     }
-
     console.log("Add contribution Successfully");
     return res.status(200).json({
       message: "Add contribution Successfully",
-      contribution: contribution, // Optionally, you can return the created contribution
+      contribution: contribution // Optionally, you can return the created contribution
     });
   } catch (error) {
     console.log("Error create contribution --: " + error);
-    res.status(500).json({ error: error.message });
+    res.status(500).json({
+      error: error.message
+    });
   }
 };
-
 const getAllContribution = async (req, res) => {
   try {
     const contribution = await Contributions.find();
-
     res.status(200).json(contribution);
     console.log("Get all contributions successfully");
   } catch (error) {
     console.log("Error get all contributions: " + error);
-    res.status(500).json({ error: error.message });
+    res.status(500).json({
+      error: error.message
+    });
   }
 };
-
 const showcontributionbyFaculty = async (req, res) => {
   try {
     const faculty = await Faculty.findById(req.body.faculty_id);
@@ -143,7 +136,7 @@ const showcontributionbyFaculty = async (req, res) => {
       throw new Error("Faculty not found, please check faculty_id");
     }
     const contribution = await Contributions.find({
-      faculty_id: faculty._id,
+      faculty_id: faculty._id
     });
     if (!contribution) {
       throw new Error("Contribution not found (find by faculty_id)");
@@ -153,25 +146,26 @@ const showcontributionbyFaculty = async (req, res) => {
     if (status) {
       const contributionByStatus = await Contributions.find({
         faculty_id: faculty._id,
-        status: status,
+        status: status
       });
       return res.status(200).json({
         EM: "success (get by status)",
         EC: 0,
-        DT: contributionByStatus,
+        DT: contributionByStatus
       });
     }
     return res.status(200).json({
       EM: "success",
       EC: 0,
-      DT: contribution,
+      DT: contribution
     });
   } catch (error) {
     console.log("Error get contribution by  --: " + error);
-    res.status(500).json({ error: error.message });
+    res.status(500).json({
+      error: error.message
+    });
   }
 };
-
 const showcontributionForGuest = async (req, res) => {
   try {
     let cookie = req.cookies;
@@ -188,27 +182,29 @@ const showcontributionForGuest = async (req, res) => {
     }
     const contribution = await Contributions.find({
       faculty_id: faculty._id,
-      status: 0,
+      status: 0
     });
     if (!contribution) {
       throw new Error("Contribution not found (find by faculty_id)");
     }
-    const contributionsFiltered = contribution.map((contribution) => {
+    const contributionsFiltered = contribution.map(contribution => {
       return {
         topic_name: contribution.topic_name,
         name: contribution.name,
         description: contribution.description,
-        document: contribution.document,
+        document: contribution.document
       };
     });
     return res.status(200).json({
       EM: "success",
       EC: 0,
-      DT: contributionsFiltered,
+      DT: contributionsFiltered
     });
   } catch (error) {
     console.log("Error get contribution by  --: " + error);
-    res.status(500).json({ error: error.message });
+    res.status(500).json({
+      error: error.message
+    });
   }
 };
 
@@ -216,9 +212,10 @@ const showcontributionForGuest = async (req, res) => {
 const downloadContribution = async (req, res) => {
   try {
     const contribution = await Contribution.findById(req.params.id);
-
     if (!contribution) {
-      return res.status(404).json({ message: "Contribution not found" });
+      return res.status(404).json({
+        message: "Contribution not found"
+      });
     }
     const files = contribution.document;
     const foundFiles = [];
@@ -227,13 +224,18 @@ const downloadContribution = async (req, res) => {
     for (const file of files) {
       try {
         await fs.access(file); // Check if the file exists
-        foundFiles.push({ path: file, name: path.basename(file) });
+        foundFiles.push({
+          path: file,
+          name: path.basename(file)
+        });
       } catch (error) {
         notFoundFiles.push(path.basename(file));
       }
     }
     if (foundFiles.length === 0) {
-      return res.status(404).json({ message: "No valid files found" });
+      return res.status(404).json({
+        message: "No valid files found"
+      });
     }
     if (notFoundFiles.length > 0) {
       console.log("Files not found:", notFoundFiles);
@@ -241,16 +243,19 @@ const downloadContribution = async (req, res) => {
     res.zip(foundFiles);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({
+      message: "Internal server error"
+    });
   }
 };
-
 const delContribution = async (req, res) => {
   try {
     const contributionId = req.params.id;
     const contribution = await Contributions.findById(contributionId);
     if (!contribution) {
-      return res.status(404).json({ message: "Contribution not found" });
+      return res.status(404).json({
+        message: "Contribution not found"
+      });
     }
     for (const file of contribution.document) {
       try {
@@ -260,24 +265,26 @@ const delContribution = async (req, res) => {
         console.error(`Error deleting file ${file}: ${error}`);
       }
     }
-    await Comments.deleteMany({ contribution_id: contributionId });
+    await Comments.deleteMany({
+      contribution_id: contributionId
+    });
     await Contributions.findByIdAndDelete(contributionId);
-
     console.log("Contribution deleted successfully");
-    return res
-      .status(200)
-      .json({ message: "Contribution deleted successfully" });
+    return res.status(200).json({
+      message: "Contribution deleted successfully"
+    });
   } catch (error) {
     console.log("Error deleting contribution: " + error);
-    return res.status(500).json({ error: error.message });
+    return res.status(500).json({
+      error: error.message
+    });
   }
 };
-
 module.exports = {
   createContribution,
   getAllContribution,
   downloadContribution,
   showcontributionbyFaculty,
   delContribution,
-  showcontributionForGuest,
+  showcontributionForGuest
 };
---
+++ build/routers/auth.Router.js
@@ -0,0 +1,5 @@
+const routerAPI = require('express').Router();
+const authController = require("../controllers/auth.Controller");
+routerAPI.post("/register", authController.Register);
+routerAPI.post("/login", authController.Login);
+module.exports = routerAPI;
