cc20274885e9abfb5bd73fe14d4b85073da49d32
KhoaTruong17 <truongndkgch190486@email.com>
2024-04-10 21:17:04 +0700

    ok

--- build/routers/auth.Router.js
+++
@@ -1,5 +0,0 @@
-const routerAPI = require('express').Router();
-const authController = require("../controllers/auth.Controller");
-routerAPI.post("/register", authController.Register);
-routerAPI.post("/login", authController.Login);
-module.exports = routerAPI;
