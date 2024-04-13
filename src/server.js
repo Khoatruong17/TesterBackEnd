4d810f96d05d69b0f7967f5f81f664fe7ea2c055
KhoaTruong17 <truongndkgch190486@email.com>
2024-04-13 14:20:57 +0700

    sửa file server

--- src/server.js
+++ src/server.js
@@ -3,59 +3,33 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const bodyParser = require("body-parser");
-const { EventEmitter } = require("events");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
-
-const app = express();
-
-// Set the view engine
+const morgan = require("morgan");
+const apiRouter = require("./routers/api.Router");
 const configViewEngine = require("./config/viewEngine");
 const connection = require("./config/database");
 
-//config bodyParser
-app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extends: true}));
-//app.use(express.urlencoded({ extended: true }))
+const app = express();
+const port = process.env.PORT || 3001;
+const hostname = process.env.HOST_NAME;
 
-//Config template engine
+// Middleware
+app.use(morgan("dev")); // Logging middleware
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser());
+app.use(fileUpload());
+app.use(cors());
+
+// Set view engine
 configViewEngine(app);
 
-app.use(express.json());
-//Config Cookie Parser
-app.use(cookieParser());
-
-// Config file to upload
-// Default config
-app.use(fileUpload());
-
 // Setup routes
-// const authRoute = require('./routers/auth.Router');
-// const groupRoute = require('./routers/group.Router');
-// const roleRoute = require('./routers/role.Router');
-// const facultyRoute = require('./routers/faculty.Router');
-// const userRoute = require('./routers/user.Router');
-const apiRouter = require("./routers/api.Router");
-
-//Router
-// app.use("/v1", authRoute);
-// app.use("/v1", groupRoute);
-// app.use("/v1", roleRoute);
-// app.use("/v1", facultyRoute);
-// app.use("/v1", userRoute);
 apiRouter(app);
 
-// Set Max Listener
-// create new EventEmitter
-const myEmitter = new EventEmitter();
-myEmitter.setMaxListeners(20); // Example, setMaxListeners to 20
-// Add listeners to event'
-for (let i = 0; i < 20; i++) {
-  myEmitter.on("event", () => {
-    console.log("Event occurred");
-  });
-}
+// CORS setup
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header(
@@ -64,31 +38,21 @@ app.use(function (req, res, next) {
   );
   next();
 });
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+
+// 404 handler
 app.use((req, res) => {
   return res.send("404 Not Found");
 });
 
-//self running function
-const port = process.env.PORT || 11000;
-const hostname = process.env.HOST_NAME;
+// Start server
 (async () => {
   try {
-    //test connection
+    // Test database connection
     await connection();
     app.listen(port, hostname, () => {
       console.log(`App is running at http://${hostname}:${port}/`);
     });
   } catch (err) {
-    console.log(">>> Error start server: " + err);
+    console.log(">>> Error starting server: " + err);
   }
 })();
-
-// Authentication function
-
-// Authorization function
